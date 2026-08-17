be21fb0880d235c2062a1b290358e9347f68544c
Rakesh Manchikanti <rakeshmanchikanti12@gmail.com>
2026-08-18 01:16:46 +0530

    Update API_URL to use production endpoint

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import "./App.css";
 
-const API_URL = "http://localhost:5000";
+const API_URL = "https://roof-estimator-api-ho7w.onrender.com";
 
 function App() {
   const [config, setConfig] = useState(null);
