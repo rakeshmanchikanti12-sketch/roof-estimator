d3f88d40dd0106c7fd2173e35f3105fae44e9ab4
Rakesh Manchikanti <rakeshmanchikanti12@gmail.com>
2026-08-18 11:36:45 +0530

    Change API URL to production endpoint

--- client/src/pages/AdminLeads.jsx
+++ client/src/pages/AdminLeads.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
-const API_URL = "http://localhost:5000";
+const API_URL = "https://roof-estimator-api-ho7w.onrender.com";
 
 function AdminLeads() {
   const [leads, setLeads] = useState([]);
